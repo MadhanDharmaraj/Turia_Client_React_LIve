05fb703fa2a24b9ee37aafc736801b27a48fce1f
Madhan Dharmaraj <madhan@turia.in>
2023-01-07 00:49:56 +0530

    pro changes

---
+++ static/js/22.ecd88606.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[22],{1041:function(a,s,d){}}]);
+//# sourceMappingURL=22.ecd88606.chunk.js.map
