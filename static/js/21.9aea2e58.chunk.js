42e82386e0d4282edabade40c65a26356397227f
Madhan Dharmaraj <madhan@turia.in>
2023-01-10 12:55:39 +0530

    Build live Jan10

---
+++ static/js/21.9aea2e58.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[21],{1041:function(a,s,d){}}]);
+//# sourceMappingURL=21.9aea2e58.chunk.js.map
