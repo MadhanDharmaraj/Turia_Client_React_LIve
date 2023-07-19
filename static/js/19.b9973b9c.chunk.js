93e4f70138793e39e96d0f7cd0a9ff8aef705166
Madhan Dharmaraj <madhan@turia.in>
2023-07-19 22:17:38 +0530

    pwa part 8 changes

--- static/js/19.b9973b9c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[19],{933:function(a,s,d){}}]);
-//# sourceMappingURL=19.b9973b9c.chunk.js.map
--- static/js/20.23472eab.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[20],{1027:function(a,s,d){}}]);
-//# sourceMappingURL=20.23472eab.chunk.js.map
