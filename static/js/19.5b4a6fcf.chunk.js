3c0e67cdf5e373c0fd4d3c9886599d033e776511
Madhan Dharmaraj <madhan@turia.in>
2023-07-20 08:13:51 +0530

    service worker pwa changes

--- static/js/19.5b4a6fcf.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[19],{934:function(a,s,d){}}]);
-//# sourceMappingURL=19.5b4a6fcf.chunk.js.map
